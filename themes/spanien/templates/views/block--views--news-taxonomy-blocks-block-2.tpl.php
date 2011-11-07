172a1a509d260668041d323bb67233edbe91924b
Brian Jensen <mail@magnify.dk>
2011-11-07 13:10:59 +0100

    Added news tpls and styling of news

---
+++ themes/spanien/templates/views/block--views--news-taxonomy-blocks-block-2.tpl.php
@@ -0,0 +1,9 @@
+<nav class="block block-secondary block-style-menu">
+  <?php print render($title_prefix); ?>
+  <?php if ($block->subject): ?>
+    <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  
+  <?php print $content ?>
+</nav>
