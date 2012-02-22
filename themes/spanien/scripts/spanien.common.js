c3d7947c3f0933365aa4a6d4dec07fe0f80a98b7
Brian Jensen <mail@magnify.dk>
2012-02-22 12:50:08 +0100

    Styling of submenu on mobile devices

--- themes/spanien/scripts/spanien.common.js
+++ themes/spanien/scripts/spanien.common.js
@@ -16,72 +16,60 @@
    * Creates <select /> from menu block.
    */
   function menuToSelect(source, destination) {
-    // Grab current selected main menu section.
-
-    // Create wrapper for mobile menu.
-    $("<div />", {
-      "class" : "mobile-menu"
-    }).appendTo(destination);
-
-    // Create menu-title
-    $("<h2 />", {
-      "class" : "menu-title block-title",
-      "text"  : Drupal.t('Pages within')
-    }).appendTo(".mobile-menu", $(destination));
-
-    // Create the dropdown base
-    $("<select />", {
-    }).appendTo(".mobile-menu", $(destination));
-
-    // Create default option "Go to..."
-    $("<option />", {
-       "selected": "selected",
-       "value"   : "",
-       "text"    : Drupal.t('Select page')
-    }).appendTo($("select", destination));
-
-    // Populate dropdown with menu items
-    $(source).each(function() {
-
-      var el = $(this);
+    
+    // Make sure there is a reason to create the menu.
+    if ($(source).find("ul").length) {
+      // Create wrapper for mobile menu.
+      $("<div />", {
+        "class" : "mobile-menu"
+      }).appendTo(destination);
       
-      
-      var hasChildren = el.find("ul"),
-          children    = el.find("li");
+      console.log($(source));
 
-      if (hasChildren.length) {
+      // Create menu-title
+      $("<h2 />", {
+        "class" : "menu-title block-title",
+        "text"  : Drupal.t('Navigation')
+      }).appendTo(".mobile-menu", $(destination));
 
-        $("<optgroup />", {
-          "label": el.find("> h2 > a").text()
-        }).appendTo($("select", destination));
+      // Create the dropdown base
+      $("<select />", {
+      }).appendTo(".mobile-menu", $(destination));
+
+      // Create default option "Go to..."
+      $("<option />", {
+         "selected": "selected",
+         "value"   : "",
+         "text"    : Drupal.t('Select page')
+      }).appendTo($("select", destination));
+
+      // Populate dropdown with menu items
+      $(source).each(function() {
+
+        var el = $(this);
+
+        children    = el.find("li");
 
         $("<option />", {
           "value" : el.find("> h2 > a").attr("href"),
           "text"  : el.find("> h2 > a").text()
-        }).appendTo("optgroup:last");
+        }).appendTo("select:last");
 
         children.find("a").each(function() {
-
           $("<option />", {
             "value" : $(this).attr("href"),
             "text": " - " + $(this).text()
-          }).appendTo("optgroup:last");
-
+          }).appendTo("select:last");
         });
 
-      } else {
+      });
 
-        $("<option />", {
-          "value"   : el.find("a").attr("href"),
-          "text"    : el.find("a").text()
-        }).appendTo($("select", destination));
-      }
-    });
-
-    // To make dropdown actually work
-    $("select", destination).change(function() {
-      window.location = $(this).find("option:selected").val();
-    });
+      // To make dropdown actually work
+      $("select", destination).change(function() {
+        window.location = $(this).find("option:selected").val();
+      });
+        
+    }
   }
 
   /**
